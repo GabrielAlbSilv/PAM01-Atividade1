60f77301408c1c7b1db52e2f7e2fb87b2ba0ed67
GabrielAlbSilv <gabrielalbuquerquemoura@gmai.com>
2021-09-14 17:46:21 -0300

    nova commit

---
+++ app/src/main/java/br/com/etecia/atividade/cadastro.java
@@ -0,0 +1,14 @@
+package br.com.etecia.atividade;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro_layout);
+    }
+}
